9893919faec4580f78398e9609d166a71e361c8d
andresito404 <agustin1luca@gmail.com>
2023-09-11 14:48:33 -0300

    Register Scroll created

--- app/src/main/java/com/cutedomain/kittyreader/screens/RegisterScreen.kt
+++ app/src/main/java/com/cutedomain/kittyreader/screens/RegisterScreen.kt
@@ -1,6 +1,7 @@
 package com.cutedomain.kittyreader.screens
 
 import android.annotation.SuppressLint
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -13,7 +14,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Lock
@@ -40,6 +43,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -117,6 +121,7 @@ fun RegisterForm(navController: NavController){
             modifier = Modifier
                 .fillMaxWidth()
                 .fillMaxHeight()
+                .verticalScroll(rememberScrollState())
                 .padding(
                     PaddingValues(
                         top = 80.dp,
@@ -291,8 +296,10 @@ fun RegisterForm(navController: NavController){
                     }
                 }
                 // ----------------------------------------------------------
+                val context=LocalContext.current
                 Button(
-                    onClick = {  },
+
+                    onClick = { Toast.makeText(context,"App en construcción", Toast.LENGTH_SHORT).show() },
                     modifier = Modifier.fillMaxWidth(),
                     colors = ButtonDefaults.elevatedButtonColors(Color(0xFFFF5E94))
                 ) {
