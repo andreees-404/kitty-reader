3369a937474c27fee5c3992faf8f6f7035912147
andresito404 <agustin1luca@gmail.com>
2023-11-19 23:17:55 -0300

    delete mapbox

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.cutedomain.kittyreader"
         minSdk = 23
         targetSdk = 34
-        versionCode = 3
-        versionName = "0.0.2"
+        versionCode = 4
+        versionName = "0.0.3"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
